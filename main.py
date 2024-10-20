f1524f1cdddbbc63e8f64f792e2dbfd7d37519d2
Mark Kowalewski <kowalewskimark25@gmail.com>
2024-10-20 22:24:12 +0000

    create basic game loop and way for player to guess make a while loop to hold the game and allow the user to guess a word

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ gameLetters = []
 gameLetters.extend(gameWord) #splits the gameword into letters
 
 userGuess = ""
+userLetters = []
+userGuessedLetters = []
 guesses = 6 #how many chances the user gets
 
-
+while userGuess != gameWord and guesses > 0:
+    print(f"the word is {len(gameWord)} letters long")
+    print(f"you've currently guessed {userGuessedLetters} and have {guesses} guesses left")
+    userChoice = int(input("1.would you like to guess the word? \n2.would you like to guess a letter?"))
+    if userChoice == 1:
+        userGuess = input("Put in the word you're guessing!")
